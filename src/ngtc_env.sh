8ecda2746358809c34eeccef86a712800321d53b
Jens Staal <staal1978@gmail.com>
2020-12-29 12:00:22 +0100

    fixed some issues with building packages

--- src/ngtc_env.sh
+++ src/ngtc_env.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-export PATH="/mnt/dest/bin:/x86_64-linux-pc-musl/bin:$PATH"
-export LIBRARY_PATH="/mnt/dest/lib:/mnt/dest/usr/lib:/x86_64-linux-pc-musl/lib:$LIBRARY_PATH"
-export CFLAGS="-I/mnt/dest/include -I/x86_64-linux-pc-musl/include -L/mnt/dest/lib -L/mnt/dest/usr/lib -L/x86_64-linux-pc-musl/lib $CFLAGS"
+export PATH="/mnt/dest/bin:/x86_64-pc-linux-musl/bin:$PATH"
+export LIBRARY_PATH="/mnt/dest/lib:/mnt/dest/usr/lib:/x86_64-pc-linux-musl/lib:$LIBRARY_PATH"
+export CFLAGS="-I/mnt/dest/include -I/mnt/dest/usr/include -I/x86_64-pc-linux-musl/include $CFLAGS"
+export LDFLAGS="-L/mnt/dest/lib -L/mnt/dest/usr/lib -L/x86_64-pc-linux-musl/lib $LDFLAGS"
 export CC="clang"
 export CXX="clang++"
 export LD="lld"
 export AR="llvm-ar"
 export NM="llvm-nm"
 export RANLIB="llvm-ranlib"
+
+exec /bin/sh
 
 
 
